552db232b82513d0405eebf361d9000f884164c5
IkkiKyomoto <kazu06851@gmail.com>
2025-03-17 22:23:14 +0900

    feat: ホーム画面でのデータフェッチ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,29 @@ type Level = {
 };
 
 export default async function Home() {
-  const stages = await getStages();
-  const user = await getUser("1e46e6a4-24fa-449b-9ef2-deef7026dc2c");
-  const level: Level = {
-    level: user.lv,
-    exp: user.exp,
-    maxExp: 100 * 1.1 ** user.lv,
-  };
-  console.log(stages);
-  console.log(user);
-  return (
-    <div className="flex justify-between w-full h-screen">
-      <div className="w-full h-full flex items-center justify-center mr-5">
-        <StageView stages={stages} />
+  try {
+    const stages = await getStages();
+    const user = await getUser("1e46e6a4-24fa-449b-9ef2-deef7026dc2c");
+    const level: Level = {
+      level: user.lv,
+      exp: user.exp,
+      maxExp: 100 * 1.1 ** user.lv,
+    };
+
+    return (
+      <div className="flex justify-between w-full h-screen">
+        <div className="w-full h-full flex items-center justify-center mr-5">
+          <StageView stages={stages} />
+        </div>
+        <LevelBadge level={level} />
       </div>
-      <LevelBadge level={level} />
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Failed to load data:", error);
+    return (
+      <div className="flex justify-center items-center w-full h-screen">
+        <p>データの読み込みに失敗しました。</p>
+      </div>
+    );
+  }
 }
