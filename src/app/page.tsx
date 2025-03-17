955382759cfe68941a3451d839065c812f4e670e
IkkiKyomoto <kazu06851@gmail.com>
2025-03-17 21:44:57 +0900

    fix: package.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import LevelBadge from '@/features/common/LevelBadge';
-import StageView from '@/features/routes/home/StageView';
+import LevelBadge from "@/features/common/LevelBadge";
+import StageView from "@/features/routes/home/StageView";
 
 export default function Home() {
   return (
-    <div className='flex justify-between w-full h-screen'>
-      <div className='w-full h-full flex items-center justify-center mr-5'>
+    <div className="flex justify-between w-full h-screen">
+      <div className="w-full h-full flex items-center justify-center mr-5">
         <StageView />
       </div>
-        <LevelBadge />
+      <LevelBadge />
     </div>
   );
 }
