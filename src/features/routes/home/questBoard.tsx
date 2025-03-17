41dc5a55ff50068900edcb80774ecab15c82311f
mac kinu <kinusample@ts.com>
2025-03-17 23:40:45 +0900

    #11 クエストボード作成

--- src/features/routes/home/questBoard.tsx
+++ src/features/routes/home/questBoard.tsx
@@ -1,11 +1,27 @@
 //クエストボード
 //中央ステージ一覧
 import React from "react";
+import Link from "next/link"
+import { Box, Heading, List, ListItem, Link as ChakraLink, Flex } from "@chakra-ui/react";
+import { GiSwordsEmblem } from "react-icons/gi";
+import Quest from "@/class/quest";
 
-export default function QuestBoard() {
+export default function QuestBoard({quests}: {quests: Quest[]}) {
   return (
-    <div>
-      <h1>クエストボード</h1>
-    </div>
+    <Box width="350px" height="100vh" bg="gray.100" p="20px" boxShadow="none">
+      <Heading as="h1" size="2xl" mb="20px" color="#000" textAlign="center">QuestBoard</Heading>
+      <List.Root style={{ listStyleType: "none", padding: 0 }}>
+        {quests.map((quest) => (
+          <List.Item key={quest.id} style={{ marginBottom: "10px" }}>
+            <Link href={`/quest/${quest.id}`} passHref>
+              <ChakraLink display="flex" alignItems="center" p="10px 15px" bg="white" borderRadius="md" textDecoration="none" color="gray.800" boxShadow="md">
+                <GiSwordsEmblem />
+                {quest.name}
+              </ChakraLink>
+            </Link>
+          </List.Item>
+        ))}
+      </List.Root>
+    </Box>
   );
 }
--- src/stories/QuestBoard.stories.ts
+++ src/stories/QuestBoard.stories.ts
@@ -6,6 +6,40 @@ import QuestBoard from "@/features/routes/home/questBoard";
 const meta: Meta<typeof QuestBoard> = {
   title: "Home/QuestBoard",
   component: QuestBoard,
+  args: {
+    quests:[
+      {
+        id: '1',
+        name: 'クエスト１',
+        num: 1,
+        type: 'normal',
+        baseExp: 120
+      },
+      {
+        id: '2',
+        name: 'クエスト2',
+        num: 3,
+        type: 'normal',
+        baseExp: 120
+      },
+      {
+        id: '3',
+        name: 'クエスト3',
+        num: 3,
+        type: 'normal',
+        baseExp: 120
+      },
+      {
+        id: '4',
+        name: 'クエスト4',
+        num: 4,
+        type: 'normal',
+        baseExp: 120
+      },
+      // questを量産
+
+    ]
+  }
 };
 
 export default meta;
