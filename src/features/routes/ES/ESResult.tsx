37f8cc4d1a62127885b8005db9955a8679dfa294
IkkiKyomoto <kazu06851@gmail.com>
2025-03-18 17:06:47 +0900

    feat: ストーリーブックでEsResultコンポーネントを表示可

--- src/features/routes/ES/ESResult.tsx
+++ src/features/routes/ES/ESResult.tsx
@@ -1,18 +1,27 @@
 // 採点結果表示
 //フォーム
-'use client';
-import React from  "react";
+"use client";
+import scoredEs from "@/class/scoredEs";
+import React from "react";
 
-export default function EsResult({ result }: { result: any }) {
+export default function EsResult({ scoredEs }: { scoredEs: scoredEs }) {
   return (
     <div>
-      <h1>ES添削結果</h1>
-      <p>クエストID: {result.questId}</p>
-      <p>ユーザーID: {result.userId}</p>
-      <p>お題: {result.topic}</p>
-      <p>内容: {result.content}</p>
-      <p>制限文字数: {result.charLimit}</p>
-      <p>採点結果: {result.score}</p>
+      <h1>採点結果</h1>
+      <p>全体の点数: {scoredEs.allScore}</p>
+      <p>修正点: {scoredEs.correction}</p>
+      <p>修正コメント: {scoredEs.correctionComment}</p>
+      <h2>カテゴリーごとの点数</h2>
+      <ul>
+        {scoredEs.categories.map((category) => (
+          <li key={category.name}>
+            <p>カテゴリー名: {category.name}</p>
+            <p>得点: {category.score}</p>
+            <p>満点: {category.fullScore}</p>
+            <p>コメント: {category.comment}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
