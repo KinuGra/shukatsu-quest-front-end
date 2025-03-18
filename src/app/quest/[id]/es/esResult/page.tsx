79d299f3776f671c6b44b11544693fb992c15703
Shin.Yamamoto <shin.yamamoto@plaid.co.jp>
2025-03-19 00:42:16 +0900

    fix: package 不足

--- src/app/quest/[id]/es/esResult/page.tsx
+++ src/app/quest/[id]/es/esResult/page.tsx
@@ -11,7 +11,7 @@ const ResultPage = () => {
   const [result, setResult] = useState<string>("");
 
   useEffect(() => {
-    console.log('テストてすと', es);
+    console.log("テストてすと", es);
     if (es.questId && es.userId && es.topic && es.content && es.charLimit) {
       postEsDone(es)
         .then((res) => {
